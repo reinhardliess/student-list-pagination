2d6d7f613639600e6ef87f3529060bd198b6c52c
Reinhard Liess <reinhard.liess@gmail.com>
2018-11-15 16:54:36 +0100

    pagination and search implemented

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# js-techdegree-project2
+# JavaScript Techdegree Project #2: Pagination & Filtering
+
+The goal of this project was to enhance the usability of a long student list (of undetermined length) by splitting it up in pages of 10 students, providing the appropriate number of buttons for navigation.
+
+It follows the principles of [unobtrusive JavaScript](https://www.w3.org/wiki/The_principles_of_unobtrusive_JavaScript) and [progressive enhancement](https://developer.mozilla.org/en-US/docs/Glossary/Progressive_Enhancement), so that the website is still usable if JavaScript is turned off or unavailable.
+
+## The following additional functionality was added
+
+* The number of students per page was stored in `NUM_ITEMS_PER_PAGE` to make it easier to potentially change it in the future.
+* Search functionality (input field and search button) is added dynamically to the HTML.
+    * It's possible to search for name and/or email address of students. The function `filterList()` adds conditionally the class `js-found` to respective `<li>` elements. 
+    * The search is activated by typing (instant search), pressing `enter`or clicking the search button.
+    * error message, styles
--- js/script.js
+++ js/script.js
@@ -1,50 +1,299 @@
+﻿"use strict";
+
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Reinhard Liess, 2018
 ******************************************/
+
+/*
+  
+  
+  
+*/
+
+
+/* Some additional conventions
+
+* constants (there's only one) are all uppercase, spaced out with underscores for readability
+* In for loop conditions, the length of collections is cached in a variable
+* Additional spaces around nested function calls 
+
+*/ 
+ 
+// global declarations
+
+// students per page
+const NUM_ITEMS_PER_PAGE = 10;
+
+// main div
+const divPage = document.querySelector('.page');
+
+// general purpose functions 
+
+// creates DOM element, sets HTML attributes in object oProps
+const createElementEx = (elementName, oProps) => {
+  const element = document.createElement(elementName);
+  
+  for (const prop in oProps) {
+      if ( oProps.hasOwnProperty(prop) ) {
+        element[prop] = oProps[prop];
+      }
+  }
+  return element;
+}
+
+// adds tag to target element as a child, sets HTML attributes in oProps
+const appendChildEx = (tag, target, oProps) => {
+  const element = createElementEx(tag, oProps);
+  
+  target.appendChild(element);
+  return element;
+} 
+
+// app specific functions
+
+// display page (parameter), hide everything else
+const showPage = (listItems, page) => { 
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+    
+  for (let i = 0, l = listItems.length; i < l; i++) {
+    const start = (page - 1) * NUM_ITEMS_PER_PAGE;
+    const end   =  start + NUM_ITEMS_PER_PAGE - 1;
 
+    listItems[i].style.display = (i >= start && i <= end) ? '' : 'none';
+  }
+  
+  
+};
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+// removes pagination buttons, necessary for switching between browse & search modes
+const removePageButtons = () => {
+  
+  const divPagination = document.querySelector('div.pagination');
+  
+  if (divPagination) {
+    const divPage = divPagination.parentNode;
+    divPage.removeChild(divPagination);
+  }
+  
+}
+
+// appends pagination buttons, no button shown in case of only one page
+const appendPageLinks = listItems => {
+
+  /* <!-- pagination HTML to create dynamically -->
+    <div class="pagination">
+    <ul>
+      <li>
+        <a class="active" href="#">1</a>
+      </li>
+    </ul>
+  </div>
+  
+ */  
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+  // calculate number of pages, prevent showing a button if there's only one page
+  const numPages = listItems.length <= NUM_ITEMS_PER_PAGE ? 
+                    0 : 
+                    Math.ceil(listItems.length / NUM_ITEMS_PER_PAGE);
+
+  
+  // necessary for switching between browse and search modes
+  removePageButtons();
+  
+  const ul = appendChildEx('div', divPage, { className: 'pagination' })
+        .appendChild( document.createElement('ul') );
+  
+  for (let i = 1; i <= numPages; i++) {
+    let anchor = appendChildEx('a', ul.appendChild( document.createElement('li') ), 
+                                 { href: '#', textContent: i} );
+    if (i === 1) {
+      anchor.className = 'active';
+    }
+    anchor.addEventListener('click', () => {
+      showPage(listItems, i);
+      
+      // change active page button/link
+      const anchors = ul.querySelectorAll('a');
+      for (let j = 0, l = anchors.length; j < l; j++) {
+        if (j === i - 1) {
+          anchors[j].classList.add('active'); 
+        } else {
+          anchors[j].classList.remove('active'); 
+        }
+      }
+      
+    });
+  }
+ 
+}
+
+// initializes pagination for browse mode
+const initBrowse = () => {
+
+  /* NB:  when coming from search mode, the class js-found might be attached
+          to all the li elements found the last time.
+          There's no need to remove them, the search mode will set them correctly
+          during a new search and they have no relevance for browse mode 
+  */
+
+  let listStudents = document.querySelectorAll('li.student-item');  
+  
+  appendPageLinks(listStudents);
+  showPage(listStudents, 1);
+}
+
+// displays error when no results were found
+const displayError = searchInput => {
+  
+  /* <div class="js-not-found">
+        <p><span>[search expression]</span> not found.</p>      
+        <p>Try​ ​searching​ ​again​ ​using​ ​a​ ​different​ ​spelling​ ​or keyword.</p>
+      </div>
+  */    
+  
+  let divError = document.querySelector('.js-not-found');
+  
+  // prevent multiple instances of the error message showing up
+  // update search string that wasn't found
+  if (divError) {
+    const span = document.querySelector('.js-not-found span');
+    
+    span.textContent = searchInput;
+  
+  } else {
+  
+    divError = createElementEx('div', { className: 'js-not-found'});
+    const ul = document.querySelector('.student-list');
+    
+    appendChildEx('p', divError, { innerHTML: `<span>${searchInput}</span> not found.`});
+    appendChildEx('p', divError, { textContent: 'Try​ ​searching​ ​again​ ​using​ ​a​ ​different​ ​spelling​ ​or keyword.'});
+    
+    divPage.insertBefore(divError, ul);
+      
+  }
+  
+}
+
+// removes error message from screen
+const removeError = () => {
+   
+  const divError = document.querySelector('.js-not-found');
+  if (divError) {
+    divPage.removeChild(divError);
+  }
+}
+
+// checks if search term matches name or email, adds class js-found if true
+const filterList = () => {
+  
+  /*  <div class="student-details">
+        <img class="avatar" src="https://randomuser.me/api/portraits/thumb/women/10.jpg">
+        <h3>elsa lahti</h3>
+        <span class="email">elsa.lahti@example.com</span>
+      </div>
+  */
+  
+  // collection of divs containing the student details
+  const divsDetails = document.querySelectorAll('.student-details');
+  
+  // search box
+  const inputSearch = document.querySelector('.student-search input');
+  let numFound = 0;
+  
+  // iterate through the student list
+  for (let i = 0, l = divsDetails.length; i < l; i++) {
+    let isFound = false;
+    let listInfo = divsDetails[i].children;
+    
+    // iterate through student-details
+    for (let j = 0, m = listInfo.length; j < m; j++) {
+      if (listInfo[j].tagName === 'H3' || listInfo[j].tagName === 'SPAN') {
+        let textToSearch = listInfo[j].textContent.toLowerCase();
+        if (textToSearch.indexOf(inputSearch.value.toLowerCase() ) > -1) {
+          isFound = true;
+        }
+      } 
+    } // end of 'for j'
+    let li = divsDetails[i].parentNode;
+    if (isFound) {
+      li.classList.add('js-found');
+      
+      // at least one student was found, remove error message, if shown
+      removeError();
+      
+      // found student is on first page, show
+      li.style.display = ++numFound <= NUM_ITEMS_PER_PAGE ? '' : 'none';
+    } else {
+      // if not found always hide
+      li.classList.remove('js-found');
+      li.style.display = 'none';
+    } 
+  } // end of 'for i'
+
+  if (numFound === 0) {
+    displayError(inputSearch.value);
+  }
+  // recreate pagination links for search
+  const listFound = document.querySelectorAll('.js-found');
+  appendPageLinks(listFound);
+  
+}
+
+// dynamically create HTML for search function
+const initSearch = () => {
+  
+  /* <!-- student search HTML to add dynamically -->
+    <div class="student-search">
+      <input title="Instant search for name or email, ESC clears input field." placeholder="Search for students...">
+      <button>Search</button>
+    </div>
+  */
+  
+  const divHeader = document.querySelector('.page-header');
+  const divSearch = appendChildEx('div', divHeader, { className: 'student-search'});
+
+  const inputSearch = appendChildEx('input',  divSearch, {type: 'text', placeholder: 'Search for students...', 
+      title: 'Instant search for name or email, ESC clears input field.'});
+  const btnSearch = appendChildEx('button', divSearch, { textContent: 'Search'} );
+  
+  // add necessary event handlers
+  btnSearch.addEventListener('click', () => {
+    filterList();
+  });
+  
+  inputSearch.addEventListener('keyup', (event) => {
+        switch (event.keyCode) {
+        // escape
+        case 27:
+          // switch back to browsing
+          inputSearch.value = '';
+          removeError();
+          initBrowse();
+          break;
+        // enter 
+        case 13: 
+          // Search
+          filterList();
+          break;
+        // 
+        default:
+          // 
+          filterList();
+          break;
+       }
+  })
+}
+
+// global execute section when website loads
+
+initSearch();
+initBrowse();
 
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
